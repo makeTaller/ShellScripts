8da9ff2bb1aa4a514bf3f444ed220a169ccf6de0
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-03 19:42:17 -0500

    Added a test to main version to not allow empty commits

--- git-main.sh
+++ git-main.sh
@@ -3,13 +3,14 @@
 # Author: Kirk Tolliver
 # Wed Aug 25 07:59:40 AM CDT 2021
 
-echo "Hello $USER, what are you committing?" && git add . && echo $?
+if [ -n "$1" ] 
+then
+			echo "Hello $USER, what are you committing?" && git add . && echo $?
+			echo "What is you commit message?" && git commit -m "$*"  >> git-main.log && echo $?
+			echo "Lets push this shit!" && git push origin main
+			sleep  10s
+			grep main git-main.log
 
-echo "What is you commit message?" && git commit -m "$*"  >> git-main.log && echo $?
-
-echo "Lets push this shit!" && git push origin main
-
-sleep  10s
-
-grep main git-main.log
-
+	 else 
+		echo " You must enter a commit message! "
+fi
